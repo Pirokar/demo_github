e5bd5c5928c835dc09f32202da97315f62e2b4fe
Ilgar <mamedaliev.ilgar@gmail.com>
2017-03-31 12:43:04 +0700

    2.98_test Доделал мультипарт

--- threads/src/main/java/im/threads/retrofit/RetrofitService.java
+++ threads/src/main/java/im/threads/retrofit/RetrofitService.java
@@ -1,30 +1,30 @@
-//package im.threads.retrofit;
-//
-//import im.threads.model.FileUploadResponse;
-//import okhttp3.MultipartBody;
-//import retrofit2.Call;
-//import retrofit2.http.GET;
-//import retrofit2.http.Header;
-//import retrofit2.http.Multipart;
-//import retrofit2.http.PUT;
-//import retrofit2.http.Part;
-//import retrofit2.http.Query;
-//
-///**
-// * Created by Admin on 28.03.2017.
-// */
-//
-//public interface RetrofitService {
-//    @Multipart
-//    @PUT("files")
-//    Call<FileUploadResponse> upload(
-//            @Part MultipartBody.Part file, @Header("X-Client-Token") String token
-//    );
-//
-//    @GET("history")
-//    Call<FileUploadResponse> history(
-//            @Header("X-Client-Token") String token,
-//            @Query("start") String start,
-//            @Query("count") String count
-//    );
-//}
+package im.threads.retrofit;
+
+import im.threads.model.FileUploadResponse;
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
+
+/**
+ * Created by Admin on 28.03.2017.
+ */
+
+public interface RetrofitService {
+    @Multipart
+    @PUT("files")
+    Call<FileUploadResponse> upload(
+            @Part MultipartBody.Part file, @Header("X-Client-Token") String token
+    );
+
+    @GET("history")
+    Call<FileUploadResponse> history(
+            @Header("X-Client-Token") String token,
+            @Query("start") String start,
+            @Query("count") String count
+    );
+}
--- threads/src/main/java/im/threads/utils/FilePoster.java
+++ threads/src/main/java/im/threads/utils/FilePoster.java
@@ -1,6 +1,8 @@
 package im.threads.utils;
 
 import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
 import android.webkit.MimeTypeMap;
 
 import com.google.gson.Gson;
@@ -12,7 +14,9 @@ import java.net.HttpURLConnection;
 
 import im.threads.model.FileDescription;
 import im.threads.model.FileUploadResponse;
-import okhttp3.Call;
+import im.threads.retrofit.RetrofitService;
+import im.threads.retrofit.ServiceGenerator;
+
 import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -21,6 +25,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
 
 /**
  * Created by yuri on 03.08.2016.
@@ -69,60 +74,7 @@ public class FilePoster {
 //    }
 
 
-//    public void post(final Callback<String, Throwable> callback) {
-//        String token = PrefUtils.getClientID(context);
-//        if (!token.isEmpty()) {
-//
-//            if (fileDescription.getFilePath() != null && new File(fileDescription.getFilePath().replaceAll("file://", "")).exists()) {
-//                RetrofitService retrofitService = ServiceGenerator.getRetrofitService();
-//                File file = new File(fileDescription.getFilePath().replaceAll("file://", ""));
-//                Uri fileUri = Uri.fromFile(file);
-//                String path = file.getPath();
-//                String mimeType = null;
-//
-//                if (path.contains(".")) {
-//                    String extension = file.getPath().substring(file.getPath().lastIndexOf(".") + 1);
-//                    mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-//                }
-//
-//                if(mimeType == null) {
-//                    mimeType = "*/*";
-//                }
-//
-//                RequestBody requestFile = RequestBody.create(MediaType.parse(mimeType), file);
-//                MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), requestFile);
-//
-//                Call<FileUploadResponse> call = retrofitService.upload(body, token);
-//                call.enqueue(new retrofit2.Callback<FileUploadResponse>() {
-//                    @Override
-//                    public void onResponse(Call<FileUploadResponse> call, Response<FileUploadResponse> response) {
-//                        if (response.body() != null && response.body().getResult() != null && !response.body().getResult().isEmpty()) {
-//                            callback.onSuccess(response.body().getResult());
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<FileUploadResponse> call, Throwable t) {
-//                        callback.onFail(t);
-//                        Log.i("++++++++", t.toString());
-//                    }
-//                });
-//            } else if (fileDescription.getFilePath() != null && !new File(fileDescription.getFilePath().replaceAll("file://", "")).exists()) {
-//                if (fileDescription.getDownloadPath() != null) {
-//                    callback.onSuccess(fileDescription.getDownloadPath());
-//                } else {
-//                    callback.onFail(new FileNotFoundException());
-//                }
-//            } else if (fileDescription.getFilePath() == null && fileDescription.getDownloadPath() != null){
-//                callback.onSuccess(fileDescription.getDownloadPath());
-//            }
-//        }
-//
-//    }
-
-
     public void post(final Callback<String, Throwable> callback) {
-        String url = "https://datastore.threads.im/files";
         String token = PrefUtils.getClientID(context);
         if (!token.isEmpty()) {
 
@@ -133,67 +85,126 @@ public class FilePoster {
             }
 
             if (file != null && file.exists() && file.isFile() && file.canRead()) {
-
-                OkHttpClient client = new OkHttpClient.Builder()
-                        .addInterceptor(new HttpLoggingInterceptor())
-                        .build();
-
+                RetrofitService retrofitService = ServiceGenerator.getRetrofitService();
                 String path = file.getPath();
                 String mimeType = null;
 
                 if (path.contains(".")) {
-                    String extension = file.getPath().substring(path.lastIndexOf(".") + 1);
+                    String extension = file.getPath().substring(file.getPath().lastIndexOf(".") + 1);
                     mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
                 }
 
-                if (mimeType == null) {
+                if(mimeType == null) {
                     mimeType = "*/*";
                 }
 
-                MediaType mediaType = MediaType.parse(mimeType);
+                RequestBody requestFile = RequestBody.create(MediaType.parse(mimeType), file);
+                MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), requestFile);
 
-                RequestBody requestBody = new MultipartBody.Builder()
-                        .setType(MultipartBody.FORM)
-                        .addFormDataPart("file", file.getName(), RequestBody.create(mediaType, file))
-                        .build();
+                Call<FileUploadResponse> call = retrofitService.upload(body, token);
 
-//                HttpUrl url = new HttpUrl.Builder()
-//                        .scheme("https")
-//                        .host("datastore.threads.im")
-//                        .addPathSegment("files")
-//                        .build();
-
-                Request request = new Request.Builder()
-                        .header("X-Client-Token", token)
-                        .url(url)
-                        .post(requestBody)
-                        .build();
-
-                client.newCall(request).enqueue(new okhttp3.Callback() {
+                call.enqueue(new retrofit2.Callback<FileUploadResponse>() {
                     @Override
-                    public void onFailure(Call call, IOException e) {
-                        callback.onFail(e);
+                    public void onResponse(Call<FileUploadResponse> call, retrofit2.Response<FileUploadResponse> response) {
+                        if (response.body() != null && response.body().getResult() != null && !response.body().getResult().isEmpty()) {
+                            callback.onSuccess(response.body().getResult());
+                        }
                     }
 
                     @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        Gson gson = new Gson();
-                        FileUploadResponse fileUploadResponse = gson.fromJson(response.body().charStream(), FileUploadResponse.class);
-                        callback.onSuccess(fileUploadResponse.getResult());
+                    public void onFailure(Call<FileUploadResponse> call, Throwable t) {
+                        callback.onFail(t);
+                        Log.i("++++++++", t.toString());
                     }
                 });
+
             } else if (fileDescription.getFilePath() != null && !new File(fileDescription.getFilePath().replaceAll("file://", "")).exists()) {
                 if (fileDescription.getDownloadPath() != null) {
                     callback.onSuccess(fileDescription.getDownloadPath());
                 } else {
                     callback.onFail(new FileNotFoundException());
                 }
-            } else if (fileDescription.getFilePath() == null && fileDescription.getDownloadPath() != null) {
+            } else if (fileDescription.getFilePath() == null && fileDescription.getDownloadPath() != null){
                 callback.onSuccess(fileDescription.getDownloadPath());
             }
         }
 
     }
+
+
+//    public void post(final Callback<String, Throwable> callback) {
+//        String url = "https://datastore.threads.im/files";
+//        String token = PrefUtils.getClientID(context);
+//        if (!token.isEmpty()) {
+//
+//            File file = null;
+//
+//            if (fileDescription.getFilePath() != null) {
+//                file = new File(fileDescription.getFilePath().replaceAll("file://", ""));
+//            }
+//
+//            if (file != null && file.exists() && file.isFile() && file.canRead()) {
+//
+//                OkHttpClient client = new OkHttpClient.Builder()
+//                        .addInterceptor(new HttpLoggingInterceptor())
+//                        .build();
+//
+//                String path = file.getPath();
+//                String mimeType = null;
+//
+//                if (path.contains(".")) {
+//                    String extension = file.getPath().substring(path.lastIndexOf(".") + 1);
+//                    mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+//                }
+//
+//                if (mimeType == null) {
+//                    mimeType = "*/*";
+//                }
+//
+//                MediaType mediaType = MediaType.parse(mimeType);
+//
+//                RequestBody requestBody = new MultipartBody.Builder()
+//                        .setType(MultipartBody.FORM)
+//                        .addFormDataPart("file", file.getName(), RequestBody.create(mediaType, file))
+//                        .build();
+//
+////                HttpUrl url = new HttpUrl.Builder()
+////                        .scheme("https")
+////                        .host("datastore.threads.im")
+////                        .addPathSegment("files")
+////                        .build();
+//
+//                Request request = new Request.Builder()
+//                        .header("X-Client-Token", token)
+//                        .url(url)
+//                        .post(requestBody)
+//                        .build();
+//
+//                client.newCall(request).enqueue(new okhttp3.Callback() {
+//                    @Override
+//                    public void onFailure(Call call, IOException e) {
+//                        callback.onFail(e);
+//                    }
+//
+//                    @Override
+//                    public void onResponse(Call call, Response response) throws IOException {
+//                        Gson gson = new Gson();
+//                        FileUploadResponse fileUploadResponse = gson.fromJson(response.body().charStream(), FileUploadResponse.class);
+//                        callback.onSuccess(fileUploadResponse.getResult());
+//                    }
+//                });
+//            } else if (fileDescription.getFilePath() != null && !new File(fileDescription.getFilePath().replaceAll("file://", "")).exists()) {
+//                if (fileDescription.getDownloadPath() != null) {
+//                    callback.onSuccess(fileDescription.getDownloadPath());
+//                } else {
+//                    callback.onFail(new FileNotFoundException());
+//                }
+//            } else if (fileDescription.getFilePath() == null && fileDescription.getDownloadPath() != null) {
+//                callback.onSuccess(fileDescription.getDownloadPath());
+//            }
+//        }
+//
+//    }
 
 
 }
