7136656d54cd42690e6d00c5baa16d3843f76475
Ilgar <mamedaliev.ilgar@gmail.com>
2017-06-07 12:34:44 +0700

    Неважная правка

--- threads/src/main/java/im/threads/fragments/ChatFragment.java
+++ threads/src/main/java/im/threads/fragments/ChatFragment.java
@@ -19,7 +19,6 @@ import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
