b82a6232428e7710342f7ce022253995d70bc8eb
Anna Voronova <anna-danekina@yandex.ru>
2017-08-08 17:48:30 +0700

    added styles for survey
    #THREADS-1559 Опросы в МП - android

--- app/src/brooma/java/com/sequenia/appwithchatdev/ChatIntentHelper.java
+++ app/src/brooma/java/com/sequenia/appwithchatdev/ChatIntentHelper.java
@@ -16,7 +16,6 @@ public class ChatIntentHelper {
                 .setDefaultFontBold("comic.ttf")
                 .setDefaultFontLight("comic.ttf")
                 .setDefaultFontRegular("comic.ttf")
-                .setSurveyFont("comic.ttf")
                 .setChatTitleStyle(R.string.contact_center,//заголовок ToolBar chatTitleTextResId
                         R.color.toolbar_background,//ToolBar background chatToolbarColorResId
                         R.color.toolbar_widget,//Toolbar widget chatToolbarTextColorResId
