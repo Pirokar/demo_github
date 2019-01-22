2375bd0413f569448146503710e869f756a93dcb
maslennikov <maslennikov@brooma.ru>
2019-01-22 17:56:54 +0700

    FileProvider conflict workaround, debug logging added
    #THREADS-4227

--- threads/src/main/java/im/threads/helpers/FileHelper.java
+++ threads/src/main/java/im/threads/helpers/FileHelper.java
@@ -1,32 +1,40 @@
 package im.threads.helpers;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.util.Log;
 
 import java.io.File;
 
+import im.threads.model.ChatStyle;
+
 public class FileHelper {
+
+    private static String LOG_TAG = FileHelper.class.getSimpleName();
 
     public static File createImageFile(Context context) {
 
         String filename = "thr" + System.currentTimeMillis() + ".jpg";
         File output = null;
-        try {
 
+        try {
             output = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
                     filename);
-        } catch (Exception e) {
-            e.printStackTrace();
-            try {
-                output = new File(context.getPackageManager().getApplicationInfo(context.getPackageName(), 0).dataDir + File.separator + filename);
-            } catch (PackageManager.NameNotFoundException e1) {
-                e1.printStackTrace();
+            if (ChatStyle.getInstance().isDebugLoggingEnabled) {
+                Log.d(LOG_TAG, "File genereated into ExternalStoragePublicDirectory, DCIM : " + output.getAbsolutePath());
             }
-
+        } catch (Exception e) {
+            Log.w(LOG_TAG, "Could not create file in public storage");
+            if (ChatStyle.getInstance().isDebugLoggingEnabled) {
+                Log.d(LOG_TAG, "", e);
+            }
         }
+
         if (output == null) {
             output = new File(context.getFilesDir(), filename);
+            if (ChatStyle.getInstance().isDebugLoggingEnabled) {
+                Log.d(LOG_TAG, "File genereated into filesDir : " + output.getAbsolutePath());
+            }
         }
 
         return output;
---
+++ threads/src/main/java/im/threads/utils/ThreadsFileProvider.java
@@ -0,0 +1,6 @@
+package im.threads.utils;
+
+import android.support.v4.content.FileProvider;
+
+public class ThreadsFileProvider extends FileProvider {
+}
