ee45cbd2f6f3a80b880a842659a0cd367c652302
maslennikov <maslennikov@brooma.ru>
2019-04-23 13:34:32 +0700

    Plurals. Added "other" as workaround for default locale, autotests added
    #THREADS-4913

---
+++ threads/src/test/java/im/threads/resources/PluralsTest.java
@@ -0,0 +1,103 @@
+package im.threads.resources;
+
+import android.content.Context;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.ParameterizedRobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import im.threads.R;
+
+@RunWith(ParameterizedRobolectricTestRunner.class)
+public class PluralsTest {
+
+    private Context appContext;
+    private final int quantity;
+
+    public PluralsTest(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Before
+    public void setup() {
+        appContext = RuntimeEnvironment.application.getApplicationContext();
+    }
+
+    @Test
+    public void checkPluralUnreadMessages() {
+        String pluralString = appContext.getResources().getQuantityString(R.plurals.threads_unread_messages, quantity, quantity);
+        Assert.assertNotNull(pluralString);
+    }
+
+    @Config(qualifiers="en")
+    @Test
+    public void checkENPluralUnreadMessages() {
+        String pluralEnString = appContext.getResources().getQuantityString(R.plurals.threads_unread_messages, quantity, quantity);
+        Assert.assertNotNull(pluralEnString);
+    }
+
+    @Test
+    public void checkPluralNewMessages() {
+        String pluralString = appContext.getResources().getQuantityString(R.plurals.threads_new_messages, quantity, quantity);
+        Assert.assertNotNull(pluralString);
+    }
+
+    @Config(qualifiers="en")
+    @Test
+    public void checkENPluralNewMessages() {
+        String pluralEnString = appContext.getResources().getQuantityString(R.plurals.threads_new_messages, quantity, quantity);
+        Assert.assertNotNull(pluralEnString);
+    }
+
+    @Test
+    public void checkPluralImages() {
+        String pluralString = appContext.getResources().getQuantityString(R.plurals.threads_images, quantity, quantity);
+        Assert.assertNotNull(pluralString);
+    }
+
+    @Config(qualifiers="en")
+    @Test
+    public void checkENPluralImages() {
+        String pluralEnString = appContext.getResources().getQuantityString(R.plurals.threads_images, quantity, quantity);
+        Assert.assertNotNull(pluralEnString);
+    }
+
+    @Test
+    public void checkPluralFiles() {
+        String pluralString = appContext.getResources().getQuantityString(R.plurals.threads_files, quantity, quantity);
+        Assert.assertNotNull(pluralString);
+    }
+
+    @Config(qualifiers="en")
+    @Test
+    public void checkENPluralFiles() {
+        String pluralEnString = appContext.getResources().getQuantityString(R.plurals.threads_files, quantity, quantity);
+        Assert.assertNotNull(pluralEnString);
+    }
+
+    @ParameterizedRobolectricTestRunner.Parameters(name = "quantity = {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1},
+                {2},
+                {4},
+                {5},
+                {8},
+                {9},
+                {10},
+                {11},
+                {22},
+                {37},
+                {59},
+                {103}
+        });
+    }
+
+}
