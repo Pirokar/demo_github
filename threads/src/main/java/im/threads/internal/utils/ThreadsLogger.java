e9c815ffa2e5fe881c1fe7ee8e37fe747375fa16
Aleksandr Plusnin <aplyusnin@mx.mfms.ru>
2020-12-21 17:20:27 +0300

    добавление тега EDNA_THREADS_LIB в логгер

--- threads/src/main/java/im/threads/internal/utils/ThreadsLogger.java
+++ threads/src/main/java/im/threads/internal/utils/ThreadsLogger.java
@@ -6,46 +6,48 @@ import im.threads.internal.Config;
 
 public final class ThreadsLogger {
 
+    private static final String PREFIX = "EDNA_THREADS_LIB: ";
+
     private ThreadsLogger() {
     }
 
     public static void v(String tag, String msg) {
         if (Config.instance.isDebugLoggingEnabled) {
-            Log.v(tag, msg != null ? msg : "");
+            Log.v(tag, PREFIX + (msg != null ? msg : ""));
         }
     }
 
     public static void d(String tag, String msg) {
         if (Config.instance.isDebugLoggingEnabled) {
-            Log.d(tag, msg != null ? msg : "");
+            Log.d(tag, PREFIX + (msg != null ? msg : ""));
         }
     }
 
     public static void d(String tag, String msg, Throwable tr) {
         if (Config.instance.isDebugLoggingEnabled) {
-            Log.d(tag, msg != null ? msg : "", tr);
+            Log.d(tag, PREFIX + (msg != null ? msg : ""), tr);
         }
     }
 
     public static void i(String tag, String msg) {
         if (Config.instance.isDebugLoggingEnabled) {
-            Log.i(tag, msg != null ? msg : "");
+            Log.i(tag, PREFIX + (msg != null ? msg : ""));
         }
     }
 
     public static void w(String tag, String msg) {
-        Log.w(tag, msg != null ? msg : "");
+        Log.w(tag, PREFIX + (msg != null ? msg : ""));
     }
 
     public static void w(String tag, String msg, Throwable tr) {
-        Log.w(tag, msg != null ? msg : "", tr);
+        Log.w(tag, PREFIX + (msg != null ? msg : ""), tr);
     }
 
     public static void e(String tag, String msg) {
-        Log.e(tag, msg != null ? msg : "");
+        Log.e(tag, PREFIX + (msg != null ? msg : ""));
     }
 
     public static void e(String tag, String msg, Throwable tr) {
-        Log.e(tag, msg != null ? msg : "", tr);
+        Log.e(tag, PREFIX + (msg != null ? msg : ""), tr);
     }
 }
