a5d4763134b60b1f282ef4bac251eb3d5e3d8ec8
Vladimir Rybalka <emessage@yandex.ru>
2022-07-08 14:39:41 +0300

    Added docs

--- threads/src/main/java/im/threads/internal/image_loading/ImageLoader.kt
+++ threads/src/main/java/im/threads/internal/image_loading/ImageLoader.kt
@@ -7,7 +7,18 @@ import android.widget.ImageView
 import java.io.File
 
 // TODO: rewrite it with one fun when everything will be in kotlin
+/**
+ * Загрузчик изображений
+ */
 interface ImageLoader {
+    /**
+     * Загружает изображение асинхронно
+     * @param imageView контейнер, в который необходимо загрузить изображение
+     * @param imageUrl url, который представляет изображение
+     * @param scales список Scale, которые применятся последовательно к ImageView
+     * @param errorDrawableResId ссылка на ресурс Drawable, который будет отображен в случае ошибки
+     * загрузки (напр., R.drawable.error_example)
+     */
     fun loadImage(
         imageView: ImageView,
         imageUrl: String?,
@@ -15,6 +26,15 @@ interface ImageLoader {
         errorDrawableResId: Int?
     )
 
+    /**
+     * Загружает изображение асинхронно
+     * @param imageView контейнер, в который необходимо загрузить изображение
+     * @param file файл, который представляет изображение
+     * @param scales список Scale, которые применятся последовательно к ImageView
+     * @param errorDrawableResId ссылка на ресурс Drawable, который будет отображен в случае ошибки
+     * загрузки (напр., R.drawable.error_example)
+     * @param modifications Список трансофрмаций, которые будут последовательно применены к ImageView
+     */
     fun loadFile(
         imageView: ImageView,
         file: File,
@@ -23,6 +43,15 @@ interface ImageLoader {
         modifications: List<ImageModifications>?
     )
 
+    /**
+     * Загружает изображение асинхронно
+     * @param imageView контейнер, в который необходимо загрузить изображение
+     * @param imageUrl url, который представляет изображение
+     * @param scales список Scale, которые применятся последовательно к ImageView
+     * @param errorDrawableResId ссылка на ресурс Drawable, который будет отображен в случае ошибки
+     * загрузки (напр., R.drawable.error_example)
+     * @param callback будет вызван, когда изображение загрузится или случится ошибка при загрузке
+     */
     fun loadImageWithCallback(
         imageView: ImageView,
         imageUrl: String?,
@@ -31,6 +60,16 @@ interface ImageLoader {
         callback: ImageLoaderCallback
     )
 
+    /**
+     * Загружает изображение асинхронно
+     * @param imageView контейнер, в который необходимо загрузить изображение
+     * @param imageUrl url, который представляет изображение
+     * @param scales список Scale, которые применятся последовательно к ImageView
+     * @param errorDrawableResId ссылка на ресурс Drawable, который будет отображен в случае ошибки
+     * загрузки (напр., R.drawable.error_example)
+     * @param modifications список трансофрмаций, которые будут последовательно применены к ImageView
+     * @param callback будет вызван, когда изображение загрузится или случится ошибка при загрузке
+     */
     fun loadWithModifications(
         imageView: ImageView,
         imageUrl: String?,
@@ -40,17 +79,35 @@ interface ImageLoader {
         callback: ImageLoaderCallback?
     )
 
+    /**
+     * Загружает Bitmap из url синхронно
+     * @param context контекст Android
+     * @param imageUrl url, который представляет изображение
+     */
     fun getBitmap(
         context: Context,
         imageUrl: String?
     ): Bitmap?
 
+    /**
+     * Загружает Bitmap из url синхронно
+     * @param context контекст Android
+     * @param imageUrl url, который представляет изображение
+     * @param modifications список трансофрмаций, которые будут последовательно применены к Bitmap
+     */
     fun getBitmap(
         context: Context,
         imageUrl: String?,
         modifications: List<ImageModifications>?
     ): Bitmap?
 
+    /**
+     * Получает Drawable из url асинхронно
+     * @param context контекст Android
+     * @param imageUrl url, который представляет изображение
+     * @param modifications список трансофрмаций, которые будут последовательно применены к Bitmap
+     * @param callback будет вызван, когда изображение загрузится или случится ошибка при загрузке
+     */
     fun getDrawableAsync(
         context: Context,
         imageUrl: String?,
@@ -58,6 +115,12 @@ interface ImageLoader {
         callback: ImageLoaderCallback
     )
 
+    /**
+     * Получает Bitmap из переданной ссылки на ресурс изображения
+     * @param context контекст Android
+     * @param resourceId ссылка на ресурс изображения (напр., R.drawable.image_example)
+     * @param modifications список трансофрмаций, которые будут последовательно применены к Bitmap
+     */
     fun getBitmapFromResource(
         context: Context,
         resourceId: Int,
@@ -65,7 +128,15 @@ interface ImageLoader {
     ): Bitmap?
 
     interface ImageLoaderCallback {
+        /**
+         * Вызывается по окончанию загрзуки изображений
+         * @param drawable загруженное изображение
+         */
         fun onImageLoaded(drawable: Drawable) {}
+
+        /**
+         * Вызывается в случае ошибки при загрзуке изображения
+         */
         fun onImageLoadError() {}
     }
 }
