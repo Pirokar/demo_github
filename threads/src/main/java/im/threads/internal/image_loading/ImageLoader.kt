d3472349198e7361aa10d21a8c7d96b3c819dcc3
Vladimir Rybalka <emessage@yandex.ru>
2022-07-09 13:16:28 +0300

    Documentation added

--- threads/src/main/java/im/threads/internal/image_loading/ImageLoader.kt
+++ threads/src/main/java/im/threads/internal/image_loading/ImageLoader.kt
@@ -6,35 +6,63 @@ import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import java.io.File
 
+/**
+ * Загрузчик изображений. Содержит поле currentImageLoader, которое определяет конкретную реализацию
+ *  и config, который определяет параметры загрузки.
+ */
 class ImageLoader private constructor() {
     private val config = ImageLoader.Config()
     private val currentImageLoader: ImageLoaderRealisation = CoilImageLoader()
 
+    /**
+     * Указывает url для загрузки
+     * @param url строка, определяющая url.
+     */
     fun load(url: String?): ImageLoader {
         config.url = url
         return this
     }
 
+    /**
+     * Указывает файл для загрузки
+     * @param file файл, который необходимо загрузить
+     */
     fun load(file: File): ImageLoader {
         config.file = file
         return this
     }
 
+    /**
+     * Указывает ресурс Drawable для загрузки
+     * @param resourceId ресурс, который необходимо загрузить
+     */
     fun load(resourceId: Int): ImageLoader {
         config.resourceId = resourceId
         return this
     }
 
+    /**
+     * Указывает ресурс Drawable, который будет отображаться в случае ошибки при загрузке
+     * @param resourceId drawable ресурс
+     */
     fun errorDrawableResourceId(resourceId: Int?): ImageLoader {
         config.errorDrawableResourceId = resourceId
         return this
     }
 
+    /**
+     * Указывает параметры ImageView.ScaleType, которые будут применены последовательно к изображению
+     * @param scales укажите через запятую необходимые scales
+     */
     fun scales(vararg scales: ImageView.ScaleType): ImageLoader {
         config.scales = scales
         return this
     }
 
+    /**
+     * Указывает параметры ImageView.ScaleType, которые будут применены последовательно к изображению
+     * @param scales список scales
+     */
     fun scales(scales: List<ImageView.ScaleType>?): ImageLoader {
         scales?.let {
             config.scales = it.toTypedArray()
@@ -42,11 +70,19 @@ class ImageLoader private constructor() {
         return this
     }
 
+    /**
+     * Указывает модификации (трансформации), которые будут применены последовательно к изображению
+     * @param modifications укажите через запятую необходимые модификации
+     */
     fun modifications(vararg modifications: ImageModifications): ImageLoader {
         config.modifications = modifications
         return this
     }
 
+    /**
+     * Указывает модификации (трансформации), которые будут применены последовательно к изображению
+     * @param modifications список модификаций
+     */
     fun modifications(modifications: List<ImageModifications>?): ImageLoader {
         modifications?.let {
             config.modifications = it.toTypedArray()
@@ -54,11 +90,20 @@ class ImageLoader private constructor() {
         return this
     }
 
+    /**
+     * Объект обратного вызова, который будет вызван после загрузки изображения или в случае ошибки
+     * @param callback содержит два метода. onImageLoaded - в случае успешной загрузки,
+     *  onImageLoadError - в случае ошибки
+     */
     fun callback(callback: ImageLoaderCallback?): ImageLoader {
         config.callback = callback
         return this
     }
 
+    /**
+     * Указывает контейнер, в который необходимо загрузить изображение
+     * @param imageView целевой контейнер
+     */
     fun into(imageView: ImageView) {
         config.imageView = imageView
         config.context = imageView.context
@@ -66,16 +111,27 @@ class ImageLoader private constructor() {
         currentImageLoader.load(config)
     }
 
+    /**
+     * Загружает Bitmap синхронно и возвращает его
+     * @param context android context
+     */
     fun getBitmapSync(context: Context): Bitmap? {
         config.context = context
         return currentImageLoader.getBitmapSync(config)
     }
 
+    /**
+     * Загружает Drawable асинхронно. Необходимо дополнительно указать callback для получения резульата
+     * @param context android context
+     */
     fun getDrawableAsync(context: Context) {
         config.context = context
         return currentImageLoader.load(config)
     }
 
+    /**
+     * Конфигурация для загрузки
+     */
     class Config {
         lateinit var context: Context
         var url: String? = null
@@ -102,6 +158,9 @@ class ImageLoader private constructor() {
     }
 
     companion object {
+        /**
+         * Возвращает объект ImageLoader
+         */
         @JvmStatic
         fun get() = ImageLoader()
     }
--- threads/src/main/java/im/threads/internal/image_loading/ImageLoaderRealisation.kt
+++ threads/src/main/java/im/threads/internal/image_loading/ImageLoaderRealisation.kt
@@ -6,6 +6,15 @@ import android.graphics.Bitmap
  * Контракт для конкретного загрузчика изображения.
  */
 interface ImageLoaderRealisation {
+    /**
+     * Загружает изображение согласно переданной конфигурации
+     * @param config конфигурация для загрузки
+     */
     fun load(config: ImageLoader.Config)
+
+    /**
+     * Загружает Bitmap синхронно согласно переданной конфигурации
+     * @param config конфигурация для загрузки
+     */
     fun getBitmapSync(config: ImageLoader.Config): Bitmap?
 }
