cd7b66f13ebf2a9ce82125e0d69768cf168f25c4
Sergey Mashukov <s.mashukov@yusmpgroup.com>
2022-07-21 14:37:18 +0300

    EC-13861. Бегунок проигрывателя зелёного цвета в синей теме. Сделал так, что бегунок, раскрашивается в цвета с состояниями кнопок (если состояния описаны в стиле) или в цвет тинта кнопок проигрывания/остановки аудио.

--- threads/src/main/java/im/threads/internal/utils/ColorsHelper.java
+++ threads/src/main/java/im/threads/internal/utils/ColorsHelper.java
@@ -19,6 +19,8 @@ import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.core.widget.ImageViewCompat;
 
+import im.threads.R;
+
 public final class ColorsHelper {
 
     public static void setStatusBarColor(Activity activity,
@@ -98,6 +100,21 @@ public final class ColorsHelper {
                         ContextCompat.getColor(context, pressedColorResId),
                         ContextCompat.getColor(context, enabledColorResId),
                         ContextCompat.getColor(context, disabledColorResId)
+                }
+        );
+    }
+
+    public static ColorStateList getSimpleColorStateList(Context context, @ColorRes int colorResId) {
+        return new ColorStateList(
+                new int[][]{
+                        new int[]{android.R.attr.state_pressed},
+                        new int[]{android.R.attr.state_enabled},
+                        new int[]{-android.R.attr.state_enabled}
+                },
+                new int[]{
+                        ContextCompat.getColor(context, colorResId),
+                        ContextCompat.getColor(context, colorResId),
+                        ContextCompat.getColor(context, R.color.threads_grey_aaa)
                 }
         );
     }
