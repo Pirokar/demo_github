8bc1c9dd585d892a196d87103b34ed9358393d37
Vladimir Rybalka <emessage@yandex.ru>
2022-08-25 18:32:02 +0300

    EC-14637: Removed UI layer from ThreadsLibBase

--- threads/src/main/java/im/threads/ui/core/ThreadsLib.kt
+++ threads/src/main/java/im/threads/ui/core/ThreadsLib.kt
@@ -2,10 +2,12 @@ package im.threads.ui.core
 
 import im.threads.ChatStyle
 import im.threads.business.config.BaseConfig
-import im.threads.business.config.BaseConfigBuilder
 import im.threads.business.core.ThreadsLibBase
+import im.threads.business.logger.LoggerEdna
 import im.threads.ui.config.Config
+import im.threads.ui.config.ConfigBuilder
 import im.threads.ui.styles.permissions.PermissionDescriptionDialogStyle
+import im.threads.ui.utils.preferences.PreferencesMigrationUi
 
 class ThreadsLib : ThreadsLibBase() {
     private val config by lazy {
@@ -38,12 +40,25 @@ class ThreadsLib : ThreadsLibBase() {
         fun getLibVersion() = ThreadsLibBase.getLibVersion()
 
         @JvmStatic
-        fun init(configBuilder: BaseConfigBuilder) = ThreadsLibBase.init(configBuilder)
+        fun init(configBuilder: ConfigBuilder) {
+            BaseConfig.instance = configBuilder.build()
+            createInstance()
+            BaseConfig.instance.loggerConfig?.let { LoggerEdna.init(it) }
+            PreferencesMigrationUi().migrateMainSharedPreferences()
+
+            ThreadsLibBase.init(configBuilder)
+        }
 
         @JvmStatic
         fun getInstance(): ThreadsLib {
             checkNotNull(libInstance) { "ThreadsLib should be initialized first with ThreadsLib.init()" }
             return libInstance as? ThreadsLib ?: ThreadsLib()
+        }
+
+        @JvmStatic
+        private fun createInstance() {
+            check(libInstance == null) { "ThreadsLib has already been initialized" }
+            setLibraryInstance(ThreadsLib())
         }
     }
 }
