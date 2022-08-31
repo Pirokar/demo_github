7fc829a43fc0a1d74573d00098c4607fbffb8404
Vladimir Rybalka <emessage@yandex.ru>
2022-08-31 18:12:20 +0300

    EC-14677: Reimplemented update logic for OpenGraph with RxJava ro remove flickering

--- threads/src/main/java/im/threads/business/ogParser/OpenGraphParser.kt
+++ threads/src/main/java/im/threads/business/ogParser/OpenGraphParser.kt
@@ -1,5 +1,7 @@
 package im.threads.business.ogParser
 
 interface OpenGraphParser {
-    fun getContents(urlToParse: String): OGData?
+    fun getContents(urlToParse: String?): OGData?
+    fun getContents(urlToParse: String?, messageText: String?): OGData
+    fun getCachedContents(urlToParse: String?): OGData?
 }
