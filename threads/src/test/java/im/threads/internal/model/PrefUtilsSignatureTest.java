00572bdcf8b46fbb23aa6670b3334487d00286f3
edna <edna-ios-bot@edna.ru>
2022-09-13 16:16:42 +0300

    disabled PrefUtilsSignatureTest

--- threads/src/test/java/im/threads/internal/model/PrefUtilsSignatureTest.java
+++ threads/src/test/java/im/threads/internal/model/PrefUtilsSignatureTest.java
@@ -1,53 +1,53 @@
-package im.threads.internal.model;
-
-
-import android.content.Context;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.ParameterizedRobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
-
-import java.util.Arrays;
-import java.util.Collection;
-
-import im.threads.business.utils.preferences.PrefUtilsBase;
-
-@RunWith(ParameterizedRobolectricTestRunner.class)
-public class PrefUtilsSignatureTest {
-
-    private Context appContext;
-    private String inputSignature;
-    private String expectedSignature;
-
-    @Before
-    public void setup() {
-        appContext = RuntimeEnvironment.application.getApplicationContext();
-    }
-
-    public PrefUtilsSignatureTest(String inputSignature, String expectedSignature){
-        this.inputSignature = inputSignature;
-        this.expectedSignature = expectedSignature;
-    }
-
-//    @Test
-//    public void saveAndGetSignature() {
-//        PrefUtilsBase.setClientIdSignature(inputSignature);
-//        String signature = PrefUtilsBase.getClientIdSignature();
+//package im.threads.internal.model;
 //
-//        Assert.assertEquals(expectedSignature, signature);
+//
+//import android.content.Context;
+//
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.ParameterizedRobolectricTestRunner;
+//import org.robolectric.RuntimeEnvironment;
+//
+//import java.util.Arrays;
+//import java.util.Collection;
+//
+//import im.threads.business.utils.preferences.PrefUtilsBase;
+//
+//@RunWith(ParameterizedRobolectricTestRunner.class)
+//public class PrefUtilsSignatureTest {
+//
+//    private Context appContext;
+//    private String inputSignature;
+//    private String expectedSignature;
+//
+//    @Before
+//    public void setup() {
+//        appContext = RuntimeEnvironment.application.getApplicationContext();
 //    }
-
-//    @ParameterizedRobolectricTestRunner.Parameters(name = "inputSignature = {0}, expectedResult = {1}")
-//    public static Collection<Object[]> data() {
-//        return Arrays.asList(new Object[][]{
-//                {null, ""},
-//                {"", ""},
-//                {"inputSignature", "inputSignature"},
-//                {"eroigjvmp458uyvb6", "eroigjvmp458uyvb6"}
-//        });
+//
+//    public PrefUtilsSignatureTest(String inputSignature, String expectedSignature){
+//        this.inputSignature = inputSignature;
+//        this.expectedSignature = expectedSignature;
 //    }
-
-}
+//
+////    @Test
+////    public void saveAndGetSignature() {
+////        PrefUtilsBase.setClientIdSignature(inputSignature);
+////        String signature = PrefUtilsBase.getClientIdSignature();
+////
+////        Assert.assertEquals(expectedSignature, signature);
+////    }
+//
+////    @ParameterizedRobolectricTestRunner.Parameters(name = "inputSignature = {0}, expectedResult = {1}")
+////    public static Collection<Object[]> data() {
+////        return Arrays.asList(new Object[][]{
+////                {null, ""},
+////                {"", ""},
+////                {"inputSignature", "inputSignature"},
+////                {"eroigjvmp458uyvb6", "eroigjvmp458uyvb6"}
+////        });
+////    }
+//
+//}
