759745b516c2fc6413519c9e0837e14cc8660a9c
Sergey Mashukov <sergey.mashukov@edna.ru>
2023-01-18 14:04:30 +0200

    EC-16162. Поправил некликабельность номеров телефона в сообщениях от оператора.

--- threads/src/main/java/im/threads/ui/holders/BaseHolder.kt
+++ threads/src/main/java/im/threads/ui/holders/BaseHolder.kt
@@ -180,11 +180,6 @@ abstract class BaseHolder internal constructor(
     ) {
         if (phrase.formattedPhrase.isNullOrBlank()) {
             textView.setText(phrase.phraseText, TextView.BufferType.NORMAL)
-            setTextWithHighlighting(
-                textView,
-                style.incomingMarkdownConfiguration.isLinkUnderlined,
-                url
-            )
         } else {
             (textView as? BubbleMessageTextView)?.let {
                 setMovementMethod(it)
@@ -194,6 +189,11 @@ abstract class BaseHolder internal constructor(
                 textView.setText(phrase.phraseText, TextView.BufferType.NORMAL)
             }
         }
+        setTextWithHighlighting(
+            textView,
+            style.incomingMarkdownConfiguration.isLinkUnderlined,
+            url
+        )
     }
 
     /**
