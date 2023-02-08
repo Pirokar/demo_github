f3e24c43d14073c2de414ba763ef45178ebee8fe
Vladimir Rybalka <emessage@yandex.ru>
2023-02-08 15:45:50 +0300

    EC-16299: Fixed duplicate system messages

---
+++ threads/src/main/java/im/threads/ui/adapters/utils/DuplicateMessagesRemover.kt
@@ -0,0 +1,33 @@
+package im.threads.ui.adapters.utils
+
+import im.threads.business.models.ChatItem
+import im.threads.business.models.SimpleSystemMessage
+
+/**
+ * Удаляет дубликаты сообщений
+ */
+class DuplicateMessagesRemover {
+    companion object {
+        /**
+         * Удаляет дубликаты системных сообщений по тексту
+         * @param listWhere список, в котором будут удалены дубликаты
+         */
+        @JvmStatic
+        fun removeDuplicateSystemMessages(listWhere: MutableList<ChatItem>) {
+            val mapOfLastElements = HashMap<String, Int>()
+            val elementsToRemove = arrayListOf<ChatItem>()
+            listWhere.indices.forEach { index ->
+                (listWhere[index] as? SimpleSystemMessage)?.let { mapOfLastElements[it.text] = index }
+            }
+            listWhere.indices.forEach { index ->
+                (listWhere[index] as? SimpleSystemMessage)?.let {
+                    if (mapOfLastElements[it.text] != index) {
+                        elementsToRemove.add(it)
+                    }
+                }
+            }
+            elementsToRemove.distinct()
+            elementsToRemove.forEach { listWhere.remove(it) }
+        }
+    }
+}
