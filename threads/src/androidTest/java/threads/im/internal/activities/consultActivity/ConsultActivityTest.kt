65b2f271507277bc096cd42f5c7f4754c1fa4de4
Vladimir <vladimir.rybalka@edna.ru>
2023-04-19 17:08:31 +0300

    EC-16985:ConsultActivityTest fix (implemented builder)

--- threads/src/androidTest/java/threads/im/internal/activities/consultActivity/ConsultActivityTest.kt
+++ threads/src/androidTest/java/threads/im/internal/activities/consultActivity/ConsultActivityTest.kt
@@ -6,6 +6,8 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import im.threads.business.models.ConsultInfo
 import im.threads.ui.activities.ConsultActivity
+import im.threads.ui.config.ConfigBuilder
+import im.threads.ui.core.ThreadsLib
 import org.junit.Rule
 import org.junit.Test
 
@@ -13,6 +15,22 @@ class ConsultActivityTest : TestCase() {
     private val avatarUrl = "https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg?w=826&t=st=1660821591~exp=1660822191~hmac=03ad70b2616db9df41c6174265f7a97806338958ef738236badc63c5b0ff1897"
     private val status = "Active"
     private val name = "Operator Alisa"
+
+    init {
+        val configBuilder = ConfigBuilder(ApplicationProvider.getApplicationContext())
+            .surveyCompletionDelay(2000)
+            .historyLoadingCount(50)
+            .isDebugLoggingEnabled(true)
+            .showAttachmentsButton()
+            .disableSSLPinning()
+            .serverBaseUrl("http://ednatest.ru")
+            .datastoreUrl("http://ednatest.ru")
+            .threadsGateUrl("wss://ednatest.ru")
+            .threadsGateProviderUid("threadsGateProviderUid")
+            .setNewChatCenterApi()
+
+        ThreadsLib.init(configBuilder as ConfigBuilder)
+    }
 
     private val intent = Intent(ApplicationProvider.getApplicationContext(), ConsultActivity::class.java).apply {
         putExtra(
