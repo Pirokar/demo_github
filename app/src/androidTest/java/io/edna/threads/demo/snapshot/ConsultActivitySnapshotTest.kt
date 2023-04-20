3187b547177b6df0b75c1c15539f65e1742127db
Vladimir <vladimir.rybalka@edna.ru>
2023-04-20 10:51:11 +0300

    EC-16985: ConsultActivity snapshot test

---
+++ app/src/androidTest/java/io/edna/threads/demo/snapshot/ConsultActivitySnapshotTest.kt
@@ -0,0 +1,43 @@
+package io.edna.threads.demo.snapshot
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.testify.ScreenshotRule
+import dev.testify.annotation.ScreenshotInstrumentation
+import im.threads.business.models.ConsultInfo
+import im.threads.ui.activities.ConsultActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ConsultActivitySnapshotTest {
+    private val avatarUrl = "https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg?w=826&t=st=1660821591~exp=1660822191~hmac=03ad70b2616db9df41c6174265f7a97806338958ef738236badc63c5b0ff1897"
+    private val status = "Active"
+    private val name = "Operator Alisa"
+
+    @get:Rule
+    val rule = ScreenshotRule(ConsultActivity::class.java).apply {
+        addIntentExtras {
+            it.putParcelable(
+                ConsultActivity.consultInfoKey,
+                ConsultInfo(
+                    photoUrl = avatarUrl,
+                    status = status,
+                    name = name
+                )
+            )
+        }
+    }
+
+    @Before
+    fun before() {
+        providePermissions()
+    }
+
+    @ScreenshotInstrumentation
+    @Test
+    fun testConsultActivity() {
+        rule.assertSame()
+    }
+}
