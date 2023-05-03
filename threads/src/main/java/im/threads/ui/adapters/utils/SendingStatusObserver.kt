932812b547fa2c551165af1befa94bb90fce8af8
Sergey Mashukov <sergey.mashukov@edna.ru>
2023-05-03 14:24:57 +0300

    EC-17216. Убрал ненужные переносы на новую строку.

--- threads/src/main/java/im/threads/ui/adapters/utils/SendingStatusObserver.kt
+++ threads/src/main/java/im/threads/ui/adapters/utils/SendingStatusObserver.kt
@@ -49,26 +49,15 @@ class SendingStatusObserver(private val chatAdapterRef: WeakReference<ChatAdapte
                                     if (index < list.size) {
                                         (list[index] as? UserPhrase)?.let { item ->
                                             val thisTime = Date().time
-                                            val notDeliveredStatus =
-                                                item.sentState == MessageStatus.SENDING ||
-                                                    item.sentState == MessageStatus.SENT
+                                            val notDeliveredStatus = item.sentState == MessageStatus.SENDING || item.sentState == MessageStatus.SENT
                                             if (notDeliveredStatus && thisTime - item.timeStamp > interval) {
                                                 val failedStatus = MessageStatus.FAILED
-                                                database.setStateOfUserPhraseByCorrelationId(
-                                                    item.id,
-                                                    failedStatus
-                                                )
+                                                database.setStateOfUserPhraseByCorrelationId(item.id, failedStatus)
                                                 item.sentState = failedStatus
                                                 indexesToUpdate.add(index)
                                                 withContext(Dispatchers.Main) {
                                                     chatUpdateProcessor.postOutgoingMessageStatusChanged(
-                                                        listOf(
-                                                            Status(
-                                                                item.id,
-                                                                item.backendMessageId,
-                                                                MessageStatus.FAILED
-                                                            )
-                                                        )
+                                                        listOf(Status(item.id, item.backendMessageId, MessageStatus.FAILED))
                                                     )
                                                 }
                                             }
