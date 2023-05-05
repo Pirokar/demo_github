502c2fd8fa1049f537972348e6ebb2fd5116b14d
Vladimir <vladimir.rybalka@edna.ru>
2023-05-05 12:35:11 +0300

    EC-17276: Removed internal camera

---
+++ threads/src/main/java/im/threads/ui/utils/CameraConstants.kt
@@ -0,0 +1,10 @@
+package im.threads.ui.utils
+
+class CameraConstants {
+    companion object {
+        const val IMAGE_EXTRA = "IMAGE_EXTRA"
+        const val FLASH_ON = 1
+        const val FLASH_OFF = 2
+        const val FLASH_AUTO = 3
+    }
+}
