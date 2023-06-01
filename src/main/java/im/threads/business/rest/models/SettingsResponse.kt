d184e8cdbd0f645bf21d6ad7cc0af90dfb2eb6a3
Vladimir <vladimir.rybalka@edna.ru>
2023-06-01 14:01:16 +0300

    EC-17159: SettingsResponse on Kotlin

--- src/main/java/im/threads/business/rest/models/SettingsResponse.kt
+++ src/main/java/im/threads/business/rest/models/SettingsResponse.kt
@@ -1,13 +1,5 @@
-package im.threads.business.rest.models;
+package im.threads.business.rest.models
 
-public class SettingsResponse {
-    private String clientNotificationDisplayType;
-
-    public String getClientNotificationDisplayType() {
-        return clientNotificationDisplayType;
-    }
-
-    public void setClientNotificationDisplayType(String clientNotificationDisplayType) {
-        this.clientNotificationDisplayType = clientNotificationDisplayType;
-    }
+class SettingsResponse {
+    var clientNotificationDisplayType: String? = null
 }
