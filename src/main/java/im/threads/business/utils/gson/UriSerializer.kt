9bd4861a3bedf741ef1edd50645c07992df2ac69
Vladimir <vladimir.rybalka@edna.ru>
2023-06-15 13:03:01 +0300

    EC-17159: UriSerializer and UriDeserializer on kotlin

--- src/main/java/im/threads/business/utils/gson/UriSerializer.kt
+++ src/main/java/im/threads/business/utils/gson/UriSerializer.kt
@@ -1,16 +1,14 @@
-package im.threads.business.utils.gson;
+package im.threads.business.utils.gson
 
-import android.net.Uri;
+import android.net.Uri
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import java.lang.reflect.Type
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-
-import java.lang.reflect.Type;
-
-public final class UriSerializer implements JsonSerializer<Uri> {
-    public JsonElement serialize(Uri src, Type typeOfSrc, JsonSerializationContext context) {
-        return new JsonPrimitive(src.toString());
+class UriSerializer : JsonSerializer<Uri> {
+    override fun serialize(src: Uri, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
+        return JsonPrimitive(src.toString())
     }
 }
