4a465fac8672ff1f3d1e419f8a4f061686e409f5
Sergey Mashukov <sergey.mashukov@edna.ru>
2023-06-29 12:22:12 +0300

    EC-17961. Добавил runOnUiThread() в двух местах для фрагмента в контроллере

--- src/main/java/im/threads/ui/controllers/ChatController.kt
+++ src/main/java/im/threads/ui/controllers/ChatController.kt
@@ -392,8 +392,10 @@ class ChatController private constructor() {
                         processSystemMessages(serverItems)
                         return@fromCallable setLastAvatars(serverItems)
                     } catch (e: Exception) {
-                        fragment?.hideProgressBar()
-                        fragment?.showWelcomeScreen(isNeedToShowWelcome)
+                        Runnable {
+                            fragment?.hideProgressBar()
+                            fragment?.showWelcomeScreen(isNeedToShowWelcome)
+                        }.runOnUiThread()
                         error(ThreadsApi.REST_TAG, "Requesting history items error", e)
                         return@fromCallable setLastAvatars(
                             database.getChatItems(
@@ -600,8 +602,10 @@ class ChatController private constructor() {
 
     fun loadHistory(fromBeginning: Boolean = true, applyUiChanges: Boolean = true) {
         if (isAllMessagesDownloaded) {
-            fragment?.hideProgressBar()
-            fragment?.showWelcomeScreen(isNeedToShowWelcome)
+            Runnable {
+                fragment?.hideProgressBar()
+                fragment?.showWelcomeScreen(isNeedToShowWelcome)
+            }.runOnUiThread()
             return
         }
         if (!chatState.isChatReady()) return
