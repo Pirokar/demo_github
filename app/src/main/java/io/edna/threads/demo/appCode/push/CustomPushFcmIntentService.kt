8e5aa8b1923cf8acd33c5a59c87e08ee62c320ba
Sergey Mashukov <sergey.mashukov@edna.ru>
2023-08-28 15:28:12 +0300

    EC-18224. Добавил фоновую инициализацию. Проверку, что инициализация прошла в демке. Обработку пуш событий при неинициализированной библиотеке.

--- app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushFcmIntentService.kt
+++ app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushFcmIntentService.kt
@@ -4,17 +4,27 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import im.threads.business.serviceLocator.core.inject
 import im.threads.ui.ChatCenterPushMessageHelper
+import im.threads.ui.core.ThreadsLib
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CustomPushFcmIntentService : FirebaseMessagingService() {
     private val chatCenterPushMessageHelper: ChatCenterPushMessageHelper by inject()
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        chatCenterPushMessageHelper.setFcmToken(token)
+        CoroutineScope(Dispatchers.IO).launch {
+            ThreadsLib.waitInitialization()
+            chatCenterPushMessageHelper.setFcmToken(token)
+        }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        chatCenterPushMessageHelper.process(this, message.data)
+        CoroutineScope(Dispatchers.IO).launch {
+            ThreadsLib.waitInitialization()
+            chatCenterPushMessageHelper.process(this@CustomPushFcmIntentService, message.data)
+        }
     }
 }
--- app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushHcmIntentService.kt
+++ app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushHcmIntentService.kt
@@ -6,21 +6,31 @@ import com.huawei.hms.push.HmsMessageService
 import com.huawei.hms.push.RemoteMessage
 import im.threads.business.serviceLocator.core.inject
 import im.threads.ui.ChatCenterPushMessageHelper
+import im.threads.ui.core.ThreadsLib
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CustomPushHcmIntentService : HmsMessageService() {
     private val chatCenterPushMessageHelper: ChatCenterPushMessageHelper by inject()
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        chatCenterPushMessageHelper.setHcmToken(token)
+        CoroutineScope(Dispatchers.IO).launch {
+            ThreadsLib.waitInitialization()
+            chatCenterPushMessageHelper.setHcmToken(token)
+        }
     }
 
     @SuppressLint("RestrictedApi")
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        chatCenterPushMessageHelper.process(
-            this,
-            CommonUtils.base64JsonStringToBundle(message.data)
-        )
+        CoroutineScope(Dispatchers.IO).launch {
+            ThreadsLib.waitInitialization()
+            chatCenterPushMessageHelper.process(
+                this@CustomPushHcmIntentService,
+                CommonUtils.base64JsonStringToBundle(message.data)
+            )
+        }
     }
 }
