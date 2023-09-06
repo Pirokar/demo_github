fa2e9fcdc67257d4f6d5c2392a376e9a2ef57e2b
Sergey Mashukov <sergey.mashukov@edna.ru>
2023-09-06 12:27:07 +0300

    EC-18224. Во внешних пуш хелперах все вернул к исходному виду.

--- app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushFcmIntentService.kt
+++ app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushFcmIntentService.kt
@@ -2,29 +2,18 @@ package io.edna.threads.demo.appCode.push
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import im.threads.business.serviceLocator.core.inject
 import im.threads.ui.ChatCenterPushMessageHelper
-import im.threads.ui.core.ThreadsLib
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class CustomPushFcmIntentService : FirebaseMessagingService() {
-    private val chatCenterPushMessageHelper: ChatCenterPushMessageHelper by inject()
+    private val chatCenterPushMessageHelper = ChatCenterPushMessageHelper()
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        CoroutineScope(Dispatchers.IO).launch {
-            ThreadsLib.waitInitialization()
-            chatCenterPushMessageHelper.setFcmToken(token)
-        }
+        chatCenterPushMessageHelper.setFcmToken(token)
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        CoroutineScope(Dispatchers.IO).launch {
-            ThreadsLib.waitInitialization()
-            chatCenterPushMessageHelper.process(this@CustomPushFcmIntentService, message.data)
-        }
+        chatCenterPushMessageHelper.process(this@CustomPushFcmIntentService, message.data)
     }
 }
--- app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushHcmIntentService.kt
+++ app/src/main/java/io/edna/threads/demo/appCode/push/CustomPushHcmIntentService.kt
@@ -4,33 +4,22 @@ import android.annotation.SuppressLint
 import com.edna.android.push_lite.utils.CommonUtils
 import com.huawei.hms.push.HmsMessageService
 import com.huawei.hms.push.RemoteMessage
-import im.threads.business.serviceLocator.core.inject
 import im.threads.ui.ChatCenterPushMessageHelper
-import im.threads.ui.core.ThreadsLib
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class CustomPushHcmIntentService : HmsMessageService() {
-    private val chatCenterPushMessageHelper: ChatCenterPushMessageHelper by inject()
+    private val chatCenterPushMessageHelper = ChatCenterPushMessageHelper()
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        CoroutineScope(Dispatchers.IO).launch {
-            ThreadsLib.waitInitialization()
-            chatCenterPushMessageHelper.setHcmToken(token)
-        }
+        chatCenterPushMessageHelper.setHcmToken(token)
     }
 
     @SuppressLint("RestrictedApi")
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        CoroutineScope(Dispatchers.IO).launch {
-            ThreadsLib.waitInitialization()
-            chatCenterPushMessageHelper.process(
-                this@CustomPushHcmIntentService,
-                CommonUtils.base64JsonStringToBundle(message.data)
-            )
-        }
+        chatCenterPushMessageHelper.process(
+            this@CustomPushHcmIntentService,
+            CommonUtils.base64JsonStringToBundle(message.data)
+        )
     }
 }
