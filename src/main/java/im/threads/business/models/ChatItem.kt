5a7e7ca9dcb634ea56001907bace12394030a79d
Vladimir <vladimir.rybalka@edna.ru>
2023-09-11 15:21:46 +0300

    EC-18670: Новое условие конечного элемента при загрузке истории
    Добавлено условие lastNotReadyAttachItem - при загрузке истории после определенного сообщения будет выбираться наименьшее: недоставленное сообщение пользователя | аттач в статусе менее, чем READY | последнее сообщение в чате

--- src/main/java/im/threads/business/models/ChatItem.kt
+++ src/main/java/im/threads/business/models/ChatItem.kt
@@ -1,7 +1,15 @@
 package im.threads.business.models
 
+import im.threads.business.models.enums.AttachmentStateEnum
+
 interface ChatItem {
     val timeStamp: Long
     fun isTheSameItem(otherItem: ChatItem?): Boolean
     val threadId: Long?
+
+    fun attachmentState(): AttachmentStateEnum {
+        return (this as? ConsultPhrase)?.fileDescription?.state
+            ?: (this as? UserPhrase)?.fileDescription?.state
+            ?: AttachmentStateEnum.ANY
+    }
 }
