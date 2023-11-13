556bb45872d625045856ec68e44cd860f3f15132
Vladimir <vladimir.rybalka@edna.ru>
2023-11-13 17:45:34 +0300

    EC-19275: Исправлен скролл к последнему сообщению
    Handler.postDelayed заменен на ListUpdateCallback во избежание багов, когда расчет через DiffUtil занимает больше заданного времени в postDelayed или во избежание излишней задержки в работе программы. К расчету необходимости скролла добавлено сравнение текущего и предыдущего размера листа

--- threads/src/main/java/im/threads/ui/fragments/ChatFragment.kt
+++ threads/src/main/java/im/threads/ui/fragments/ChatFragment.kt
@@ -42,6 +42,7 @@ import androidx.core.util.ObjectsCompat
 import androidx.core.view.ViewCompat
 import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListUpdateCallback
 import androidx.recyclerview.widget.RecyclerView
 import com.annimon.stream.Optional
 import com.annimon.stream.Stream
@@ -1708,8 +1709,8 @@ class ChatFragment :
         }
         chatAdapter?.let { chatAdapter ->
             val oldAdapterSize = chatAdapter.list.size
-            val isBottomItemsVisible = chatAdapter.itemCount - 1 - lastVisibleItemPosition < INVISIBLE_MESSAGES_COUNT
             val layoutManager = binding?.chatItemsRecycler?.layoutManager as LinearLayoutManager?
+            val isBottomItemsVisible = chatAdapter.itemCount - 1 - lastVisibleItemPosition < INVISIBLE_MESSAGES_COUNT
             if (layoutManager == null || list.size == 1 && list[0] is ConsultTyping || isInMessageSearchMode) {
                 return
             }
@@ -1720,35 +1721,63 @@ class ChatFragment :
             } else {
                 null
             }
-            addItemsToChat(list, isBottomItemsVisible)
-            val newAdapterSize = chatAdapter.list.size
-            if (oldAdapterSize == 0 || (forceScrollToTheEnd && !isStartSecondLevelScreen())) {
-                handler.postDelayed({ scrollToPosition(chatAdapter.itemCount - 1, false) }, 100)
-                afterResume = false
-                resumeAfterSecondLevelScreen = false
-                return
-            }
-
-            var needScrollDown = false
-            if (list.isNotEmpty()) {
-                val currLastTimeStamp = chatAdapter.list.last().timeStamp
-                val newLastTimeStamp = list.last()?.timeStamp ?: 0
-                needScrollDown = newLastTimeStamp > currLastTimeStamp && !isStartSecondLevelScreen()
-            }
-            if (afterResume) {
-                if ((!isStartSecondLevelScreen() && isBottomItemsVisible && newAdapterSize != oldAdapterSize) || needScrollDown) {
-                    scrollToPosition(chatAdapter.itemCount - 1, false)
-                } else if (lastVisibleItemTimestamp != null) {
-                    layoutManager.scrollToPosition(chatAdapter.getPositionByTimeStamp(lastVisibleItemTimestamp))
+            val listUpdatedCallback = object : ListUpdateCallback {
+                override fun onInserted(position: Int, count: Int) {
+                    updateChatAdapter(
+                        chatAdapter,
+                        layoutManager,
+                        list,
+                        oldAdapterSize,
+                        forceScrollToTheEnd,
+                        isBottomItemsVisible,
+                        lastVisibleItemTimestamp
+                    )
                 }
-            } else if ((isBottomItemsVisible && newAdapterSize > oldAdapterSize) || needScrollDown) {
-                handler.postDelayed({ scrollToPosition(chatAdapter.itemCount - 1, false) }, 100)
+
+                override fun onRemoved(position: Int, count: Int) {}
+                override fun onMoved(fromPosition: Int, toPosition: Int) {}
+                override fun onChanged(position: Int, count: Int, payload: Any?) {}
+            }
+            addItemsToChat(list, listUpdatedCallback, isBottomItemsVisible)
+        }
+    }
+
+    private fun updateChatAdapter(
+        chatAdapter: ChatAdapter,
+        layoutManager: LinearLayoutManager,
+        list: List<ChatItem?>,
+        oldAdapterSize: Int,
+        forceScrollToTheEnd: Boolean,
+        isBottomItemsVisible: Boolean,
+        lastVisibleItemTimestamp: Long?
+    ) {
+        val newAdapterSize = chatAdapter.list.size
+        if (oldAdapterSize == 0 || (forceScrollToTheEnd && !isStartSecondLevelScreen())) {
+            scrollToPosition(chatAdapter.itemCount - 1, false)
+            afterResume = false
+            resumeAfterSecondLevelScreen = false
+            return
+        }
+
+        var needScrollDown = false
+        if (list.isNotEmpty()) {
+            needScrollDown = oldAdapterSize < newAdapterSize ||
+                (list.last()?.timeStamp ?: 0) > chatAdapter.list.last().timeStamp &&
+                !isStartSecondLevelScreen()
+        }
+        if (afterResume) {
+            if ((!isStartSecondLevelScreen() && isBottomItemsVisible && newAdapterSize != oldAdapterSize) || needScrollDown) {
+                scrollToPosition(chatAdapter.itemCount - 1, false)
             } else if (lastVisibleItemTimestamp != null) {
                 layoutManager.scrollToPosition(chatAdapter.getPositionByTimeStamp(lastVisibleItemTimestamp))
             }
-            afterResume = false
-            resumeAfterSecondLevelScreen = false
+        } else if ((isBottomItemsVisible && newAdapterSize > oldAdapterSize) || needScrollDown) {
+            scrollToPosition(chatAdapter.itemCount - 1, false)
+        } else if (lastVisibleItemTimestamp != null) {
+            layoutManager.scrollToPosition(chatAdapter.getPositionByTimeStamp(lastVisibleItemTimestamp))
         }
+        afterResume = false
+        resumeAfterSecondLevelScreen = false
     }
 
     fun setStateConsultConnected(info: ConsultInfo?) = binding?.apply {
@@ -2761,14 +2790,18 @@ class ChatFragment :
         }
     }
 
-    private fun addItemsToChat(list: List<ChatItem?>?, withAnimation: Boolean = true) {
+    private fun addItemsToChat(
+        list: List<ChatItem?>?,
+        listUpdatedCallback: ListUpdateCallback? = null,
+        withAnimation: Boolean = true
+    ) {
         list?.filterNotNull()?.let {
             if (it.isNotEmpty()) {
                 val isChatReady = chatController.isChatReady()
                 val isAnimationEnabled = withAnimation && isChatReady
                 chatController.removeCorruptedFiles(it) {
                     chatController.setFormattedDurations(it, mediaMetadataRetriever) {
-                        chatAdapter?.addItems(it, isAnimationEnabled)
+                        chatAdapter?.addItems(it, listUpdatedCallback, isAnimationEnabled)
                         if (isChatReady) {
                             hideErrorView()
                             hideProgressBar()
