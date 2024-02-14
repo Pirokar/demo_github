c4ae7aa1f3c73b4540a4c6f6d189c8823bac74d4
Sergey Mashukov <sergey.mashukov@edna.ru>
2024-02-14 15:36:39 +0200

    EC-20179: Добавлен запрос на обновления HCM токена
    Кроме того, обновлены deprecated методы и классы при запросе HCM токена.

--- app/src/main/java/io/edna/threads/demo/appCode/push/HCMTokenRefresher.kt
+++ app/src/main/java/io/edna/threads/demo/appCode/push/HCMTokenRefresher.kt
@@ -1,16 +1,15 @@
 package io.edna.threads.demo.appCode.push
 
 import android.content.Context
-import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.agconnect.AGConnectOptionsBuilder
 import com.huawei.hms.aaid.HmsInstanceId
 import com.huawei.hms.common.ApiException
 import im.threads.ui.ChatCenterPushMessageHelper
 import java.io.IOException
 
 object HCMTokenRefresher {
-
     fun requestToken(context: Context) {
-        val hcmAppId = AGConnectServicesConfig.fromContext(context).getString("client/app_id")
+        val hcmAppId = AGConnectOptionsBuilder().build(context).getString("client/app_id")
         if (hcmAppId != null) {
             try {
                 val hmsInstanceId = HmsInstanceId.getInstance(context)
