b6ec062c2b3fa06eedd4211a9dcc2dfc25e912d4
Vladimir <vladimir.rybalka@edna.ru>
2024-04-18 14:35:22 +0300

    NO-TASK: Удален ручной процессинг пушей

--- app/src/main/java/edna/chatcenter/demo/appCode/push/CustomPushFcmIntentService.kt
+++ app/src/main/java/edna/chatcenter/demo/appCode/push/CustomPushFcmIntentService.kt
@@ -2,18 +2,15 @@ package edna.chatcenter.demo.appCode.push
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import edna.chatcenter.ui.visual.ChatCenterPushMessageHelper
+import edna.chatcenter.demo.appCode.EdnaChatCenterApplication
 
 class CustomPushFcmIntentService : FirebaseMessagingService() {
-    private val chatCenterPushMessageHelper = ChatCenterPushMessageHelper()
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        chatCenterPushMessageHelper.setFcmToken(token)
+        val application = applicationContext as EdnaChatCenterApplication
+        application.chatCenterUI?.setFcmToken(token)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-        chatCenterPushMessageHelper.process(message.data)
-    }
+    override fun onMessageReceived(message: RemoteMessage) {}
 }
--- app/src/main/java/edna/chatcenter/demo/appCode/push/HCMTokenRefresher.kt
+++ app/src/main/java/edna/chatcenter/demo/appCode/push/HCMTokenRefresher.kt
@@ -1,20 +1,19 @@
 package edna.chatcenter.demo.appCode.push
 
-import android.content.Context
 import com.huawei.agconnect.AGConnectOptionsBuilder
 import com.huawei.hms.aaid.HmsInstanceId
 import com.huawei.hms.common.ApiException
-import edna.chatcenter.ui.visual.ChatCenterPushMessageHelper
+import edna.chatcenter.demo.appCode.EdnaChatCenterApplication
 import java.io.IOException
 
 object HCMTokenRefresher {
-    fun requestToken(context: Context) {
-        val hcmAppId = AGConnectOptionsBuilder().build(context).getString("client/app_id")
+    fun requestToken(application: EdnaChatCenterApplication) {
+        val hcmAppId = AGConnectOptionsBuilder().build(application).getString("client/app_id")
         if (hcmAppId != null) {
             try {
-                val hmsInstanceId = HmsInstanceId.getInstance(context)
+                val hmsInstanceId = HmsInstanceId.getInstance(application)
                 val token = hmsInstanceId.getToken(hcmAppId, "HCM")
-                ChatCenterPushMessageHelper().setHcmToken(token)
+                application.chatCenterUI?.setFcmToken(token)
             } catch (e: IOException) {
             } catch (e: ApiException) {
             }
